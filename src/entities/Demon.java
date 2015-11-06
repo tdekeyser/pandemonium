084cc7e8c47224e13d77833b56fc22b58d70d4e7
tom <tom_dekeyser_7@hotmail.com>
2015-11-06 22:11:13 +0100

    Tom: first commit

---
+++ src/entities/Demon.java
@@ -0,0 +1,14 @@
+package entities;
+
+public abstract class Demon {
+	String name;
+	String gender;
+	Demon partner;
+	int age;
+	int cruelty;
+
+	public Demon() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/entities/Moveable.java
@@ -0,0 +1,7 @@
+package entities;
+
+// Interface for any entity that can move
+interface Moveable {	
+//	returns tuple of new position
+	int[] change_position();
+}
